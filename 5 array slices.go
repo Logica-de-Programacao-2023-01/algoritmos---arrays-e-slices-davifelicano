4e8144c9f19aca7ffd7e8e6fa52e3445eae62175
Davi Feliciano <feeliciaanoo@gmail.com>
2023-04-11 14:44:20 -0300

    5 array slices

--- 5 array slices.go
+++ 5 array slices.go
@@ -1,5 +1,29 @@
+// Crie um Array bidimensional de inteiros com 3 linhas e 2 colunas.
+// Solicite ao usuário que informe os valores de cada elemento da matriz.
+// Em seguida, imprima a matriz resultante.
 package main
+
+import "fmt"
 
 func main() {
 
+	matriz := [3][2]int{}
+
+	for l := 0; l < 3; l++ {
+		for c := 0; c < 2; c++ {
+			fmt.Printf("digite os valores da matriz [%d][%d]", l, c)
+			fmt.Scan(&matriz[l][c])
+		}
+	}
+
+	fmt.Println("matrizresultante")
+	for l := 0; l < 3; l++ {
+		for c := 0; c < 2; c++ {
+			fmt.Printf("%d", matriz[l][c])
+
+		}
+		fmt.Println()
+
+	}
+	fmt.Println("Essas são as atribuiçoes das matrizes ")
 }
